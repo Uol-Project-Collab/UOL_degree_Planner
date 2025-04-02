3117a9391fee2c53ee6e246862e3d498f8a2800e
Avi Miller <avi.miller22@gmail.com>
2025-04-02 22:13:28 +0100

    Initial backend structure and setup

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import routes from "./routes/index.js"
+
+const app = express();
+
+app.use("/", routes);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
