d5dd3f8eedf0936576d9b363fa900db898b9dd81
PriceyC <cameronpriceofficial@gmail.com>
2025-04-11 21:22:27 +0100

    Lvl 4 content and DS created
    -next need to add logic to change selected/displayed module
    -finish rest of the DS off

---
+++ Frontend/js_files/lvl456.js
@@ -0,0 +1,176 @@
+const lvl4 = [
+    {
+        id: 1,
+        title: "Introduction to programming I",
+        description: "By taking this module, you will learn how to use the basic elements of computer programming such as variables, conditionals, functions and loops. You will also learn how to create interactive, graphical computer programs. You will also be introduced to basic object-oriented programming techniques.",
+        credits: "15 (150 hours)",
+        assessment: ["Coursework I (50%", "Coursework II (50%)"]
+    },
+    {
+        id: 2,
+        title: "Introduction to programming II",
+        description: "This module is focused on adding to the basic programming skill set you developed in Introduction to Programming I and giving you experience working with existing code and third-party libraries. By taking this module, you will learn how to customise existing code, to implement basic object orientation and to work with a range of third-party libraries.",
+        credits: "15 (150 hours)",
+        assessment: ["Coursework I (30%", "Coursework II (70%)"]
+    },
+    {
+        id: 3,
+        title: "Computational mathematics",
+        description: "This module helps you hone your skills in thinking abstractly. It also introduces you to many of the standard continuous models used to help understand and design computational systems. Through this module, you will develop the fundamental computational mathematical tools that will support you throughout the BSc programme. Particular attention is paid to notions of experimentation, reasoning, and generalisation. By taking this module, you will learn a wide range of the computational mathematical concepts and techniques that underpin Computer Science. In particular, you will study number systems, special functions, graphing and linear algebra.",
+        credits: "15 (150 hours)",
+        assessment: ["Coursework (50%)", "Examination (50%)"]
+    },
+    {
+        id: 4,
+        title: "Discrete mathematics",
+        description: "This module helps you to hone your skills in thinking abstractly. It also introduces you to many of the standard discrete models used to help understand and design computational systems. Through this module, you will develop the fundamental discrete mathematical tools that will support you throughout the BSc programme. Particular attention is paid to notions of experimentation, reasoning, and generalisation.",
+        credits: "15 (150 hours)",
+        assessment: ["Coursework (50%)", "Examination (50%)"]
+    },
+    {
+        id: 5,
+        title: "How computers work",
+        description: "This module aims to help you understand, and to interact with, computer systems. You will learn how to use knowledge about computational processes to analyse and explain the behaviour of computer systems.The module will use the concept of a Notional Machine, an abstract representation of the functioning of a computer system, to help you to reason about computer systems and to predict their behaviour. You will also learn about typical computer system architectures, basic networking and network services such as databases.",
+        credits: "15 (150 hours)",
+        assessment: ["Coursework (50%)", "Examination (50%)"]
+    },
+    {
+        id: 6,
+        title: "Fundamentals of computer science",
+        description: "By taking this module, you will gain a broad understanding of many of the key topic areas in computer science and the fundamental concepts that underpin them. In the area of fundamental concepts, you will study binary representations and logic, complexity theory and theories of computation, finite state machines and Turing machines. Building on this, you will then study key areas of interest in computer science including databases, artificial intelligence, and machine learning. These will be presented in the light of practical examples to illustrate how they are implemented in modern computer systems.",
+        credits: "15 (150 hours)",
+        assessment: ["Coursework (50%)", "Examination (50%)"]
+    },
+    {
+        id: 7,
+        title: "Web development",
+        description: "The Web Development module equips you with the skills to design and implement dynamic web applications using foundational web programming concepts and frameworks. The course considers practical skills such as responsive design with CSS as well as the abstract underpinnings of markup languages. The course extends beyond general web programming and into areas of professional practice.",
+        credits: "15 (150 hours)",
+        assessment: ["Coursework I (30%)", "Coursework II (70%)"]
+    },
+    {
+        id: 8,
+        title: "Algorithms and data structures I",
+        description: "This module aims to help you to develop your analytical and problem-solving skills, particularly concerning thinking algorithmically. The module will encourage you to start thinking about how to use computers to solve problems. You will develop skills in thinking algorithmically and learn the central concepts of algorithms and data structures.You will learn about linear data structures such as arrays, vectors and lists, and a unifying framework for considering such data structures as collections. You will learn how algorithms can be expressed as flowcharts and pseudocode, and how to convert these expressions into running programs.You will learn specific algorithms used for sorting and searching, and how to express repetition as iteration and recursion. You will learn a simple model for execution of computation, and how to describe computational problems and their solutions. The model will allow you to compare algorithms regarding their correctness and regarding their efficiency.",
+        credits: "15 (150 hours)",
+        assessment: ["Coursework (50%)", "Examination (50%)"]
+    },
+]
+
+const lvl5 = [
+    {
+        id: 1,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 2,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 3,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 4,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 5,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 6,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 7,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 8,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+]
+
+const lvl6 = [
+    {
+        id: 1,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 2,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 3,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 4,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 5,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 6,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 7,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+    {
+        id: 8,
+        title: "",
+        description: "",
+        credits: "15 (150 hours)",
+        assessment: ["", ""]
+    },
+]
