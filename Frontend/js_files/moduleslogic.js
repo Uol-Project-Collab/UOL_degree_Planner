c9adccec612a84b949f15ec6f5f48b632f51601a
PriceyC <cameronpriceofficial@gmail.com>
2025-04-12 20:33:36 +0100

    stylings for all 3 pages

--- Frontend/js_files/moduleslogic.js
+++ Frontend/js_files/moduleslogic.js
@@ -1,13 +1,21 @@
+//gets the module list element and then grabs the level attribute, so we know what page the user is on
 let moduleList = document.getElementById('module_list');
 level = moduleList.getAttribute('level')
 
+//function will take the level variable as an argument as the page prop.
 const handleSelect = (page) => {
+    //gets the module details element to update the selected module inside
     let moduleDetails = document.getElementById('module_details')
 
+    //add a click listener to the module list
     moduleList.addEventListener('click', (e) => {
+        //checks whether the user is clicking on a 'LI' item
         if(e.target.tagName === 'LI'){
+            //gets the id and converts to a number
             let moduleId = parseInt(e.target.getAttribute('id'));
+            //looks through the module data object from lvl456.js and uses page/level to choose the module level, then find the matching id
             let selectedModule = modules[page].find(item => item.id === moduleId)
+            //safety check in case of error
             if(selectedModule) {
                 console.log(`module: ${selectedModule.title}`);
             } else {
@@ -15,30 +23,31 @@ const handleSelect = (page) => {
                 return;
             }
 
+            //updates the inner html of the module details to the selected module data
             moduleDetails.innerHTML = `
-                <h2>
+                <h2 class="text-bold text-2xl text-white text-shadow-lg">
                     ${selectedModule.title}
                 </h2>
-                <p>
+                <p class="bg-white p-4 m-2 rounded-md">
                     ${selectedModule.description}
                 </p>
-                <div>
-                    <section>
-                    <h3>
+                <div class="flex justify-between m-4 h-full">
+                    <section class="self-end space-y-2">
+                    <h3 class="text-bold">
                         Credits
                     </h3>
-                    <p>
+                    <p class="text-white">
                         ${selectedModule.credits}
                     </p>
                     </section>
-                    <section>
-                        <h3>
+                    <section class="self-end space-y-2">
+                        <h3 class="text-bold">
                             Assessment
                         </h3>
-                        <p>
+                        <p class="text-white">
                             ${selectedModule.assessment[0]}
                         </p>
-                         <p>
+                         <p class="text-white">
                             ${selectedModule.assessment[1]}
                         </p>
                     </section>
@@ -47,4 +56,5 @@ const handleSelect = (page) => {
     })
 }
 
+//call the function with the correct level passed in
 handleSelect(level);
